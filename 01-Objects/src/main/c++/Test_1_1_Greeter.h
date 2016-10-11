93eaea362cdfd13907e208824443b4127aaa09d5
Anders Arnholm <Anders.Arnholm@hiq.se>
2016-10-11 20:07:17 +0200

    Code: 1.1 Greeter function

---
+++ 01-Objects/src/main/c++/Test_1_1_Greeter.h
@@ -0,0 +1,17 @@
+//
+// Created by balp on 2016-10-11.
+//
+
+#ifndef BASICCPPENVIRONMENT_TEST_1_1_GREETER_H
+#define BASICCPPENVIRONMENT_TEST_1_1_GREETER_H
+
+
+class Test_1_1_Greeter {
+public:
+    std::string greet() {
+        return "Hello Name!";
+    }
+};
+
+
+#endif //BASICCPPENVIRONMENT_TEST_1_1_GREETER_H
